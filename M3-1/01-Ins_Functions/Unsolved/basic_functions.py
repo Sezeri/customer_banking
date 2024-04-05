a66a5b17e641157879ba57ef395685168bf0d3fc
Prudence Hessou <phessou@blueorigin.com>
2024-04-04 19:50:11 -0700

    The files downloaded are being pushed to Github

---
+++ M3-1/01-Ins_Functions/Unsolved/basic_functions.py
@@ -0,0 +1,13 @@
+# Define the hello function.
+def hello():
+    print("Hi! This is the hello function.")
+
+# Call the hello function
+hello()
+
+# Define the scream function.
+def scream():
+    print("Haaaaaa")
+
+# Call the hello function
+scream()
